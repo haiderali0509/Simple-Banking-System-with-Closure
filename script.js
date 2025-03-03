089bcac5389a3b7c90309ca62cbf2e9eb21c21ae
haiderali0509 <chhaiderali0509@gmail.com>
2025-03-03 23:57:31 +0500

    Make simple banking functions that deposit,withdraw and give balance detail and we can't access balance directly due to closure

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ function bankAccount(initialBalance) {
         deposit: function (amount) {
             balance += amount;
             console.log(`Deposited: ${amount} | Balance: ${balance}`);
+        },
+        withdraw: function (amount) {
+            if (amount > balance) {
+                console.log(`Insufficient funds!`);
+                return;
+            }
+            balance -= amount;
+            console.log(`Withdrawn:${amount} | Balance: ${balance}`);
+        },
+        getBalance: function () {
+            return balance;
         }
-    }
+    };
 }
+const myAccount = bankAccount(1600);
+myAccount.deposit(400);
+myAccount.withdraw(300);
+console.log(myAccount.getBalance());
+
+// if we try to directly access balance, we will get an error
